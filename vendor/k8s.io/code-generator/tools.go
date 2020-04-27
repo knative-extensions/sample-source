f137216bed6f1d35e35dc415b8ad87d5c361a1b2
Scott Nichols <snichols@vmware.com>
2020-04-27 10:37:16 -0700

    more tools from top level

--- vendor/k8s.io/code-generator/tools.go
+++ vendor/k8s.io/code-generator/tools.go
@@ -1,13 +1,13 @@
 // +build tools
 
 /*
-Copyright 2020 The Knative Authors.
+Copyright 2019 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-		http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,20 +16,20 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// This package contains code generation utilities
+// This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools
 
 import (
-	// _ "knative.dev/pkg/hack"
-	// TODO: use above when pkg go mod lands. For now, inline it.
-	// vvvv
-	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/client-gen"
+	_ "k8s.io/code-generator/cmd/conversion-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/defaulter-gen"
+	_ "k8s.io/code-generator/cmd/go-to-protobuf"
+	_ "k8s.io/code-generator/cmd/import-boss"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
-	_ "k8s.io/kube-openapi/cmd/openapi-gen"
-	_ "knative.dev/pkg/codegen/cmd/injection-gen"
-	_ "knative.dev/test-infra/scripts"
-	// ^^^^
+	_ "k8s.io/code-generator/cmd/openapi-gen"
+	_ "k8s.io/code-generator/cmd/register-gen"
+	_ "k8s.io/code-generator/cmd/set-gen"
 )
